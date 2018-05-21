59f42c7e7cf43728a50a7f16a109a2ade9bfb83f
Flavian DESVERNE <flavian.desverne@epitech.eu>
2018-05-21 13:44:58 +0200

    [Mobile] Added tipsi-stripe as dependency

--- mobile/android/app/src/main/java/com/aromaclop/MainApplication.java
+++ mobile/android/app/src/main/java/com/aromaclop/MainApplication.java
@@ -3,6 +3,7 @@ package com.aromaclop;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.gettipsi.stripe.StripeReactPackage;
 import com.beefe.picker.PickerViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new StripeReactPackage(),
             new PickerViewPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
