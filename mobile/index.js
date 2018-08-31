a6b1829cb4df6881885bb59d3a87d48640d2afef
DupK <jules.dupont@epitech.eu>
2018-08-31 23:10:38 +0200

    [Mobile] A function to hide warnings (painful with a tabbar).

--- mobile/index.js
+++ mobile/index.js
@@ -1,6 +1,12 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 import StoryBookUI from './storybook';
+
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Remote debugger',
+  'Module RCTImageLoader',
+]);
 
 //Return either App or StoryBookUI
 AppRegistry.registerComponent('Aromaclop', () => App);
